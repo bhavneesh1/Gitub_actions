a908853eb0c7a45c3783027df4d5336a7db4ec31
bhavneesh1 <31878087+bhavneesh1@users.noreply.github.com>
2025-05-20 23:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+import requests
+WEBHOOK_URL = "https://webhook.site/e2223437-27df-4118-8324-0cbaf4358621"  # <-- Replace with your own
+data="This is main branch"
+# Send POST request
+try:
+    response = requests.post(WEBHOOK_URL, data=message)
+    print(f"POST Status: {response.status_code}")
+except Exception as e:
+    print(f"Error posting message: {e}")
